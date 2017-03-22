819cba5ff3dfb8d6827ca8b40efd6b8da370765f
Mash Ahmed <doohsam@gmail.com>
2017-03-22 16:33:43 +0000

    Check correct Node version

--- JakeFile.js
+++ JakeFile.js
@@ -1,8 +1,22 @@
 (function() {
     "use strict";
 
+    var EXPECTED_NODE_VERSION = "v6.9.1";
     desc("Default build");
-    task("default", function() {
+    task("default",["version"], function() {
         console.log("\n\nBUILD OK");
+    });
+
+    desc("Check Node version")
+    task("version",function(){
+
+        console.log("Checking Node version: .");
+        let actualVersion = process.version;
+        if (actualVersion !== EXPECTED_NODE_VERSION){
+
+         fail("Incorrect Node version: expected " + EXPECTED_NODE_VERSION);
+        }
+
+
     });
 }());
