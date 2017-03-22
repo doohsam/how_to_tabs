f1d793c9337d199b5a4ba095989ce4e7ec786ffc
Mash Ahmed <doohsam@gmail.com>
2017-03-22 14:28:56 +0000

    Added npm rebuild

--- jake.cmd
+++ jake.cmd
@@ -1,8 +1,8 @@
 @echo off
 
-if not exist node_modules\.bin\jake{
+if not exist node_modules\.bin\jake {
 
 echo Building npm modules:
 npm rebuild
 }
-node_modules/.bin/jake %*
+call node_modules/.bin/jake %*
