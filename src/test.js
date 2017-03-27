671117d03cc1d100cd94007b2edfc6c98278845f
Mash Ahmed <doohsam@gmail.com>
2017-03-27 15:34:08 +0100

    Added testing to JakeFile

--- src/test.js
+++ src/test.js
@@ -1,18 +1,22 @@
 (function() {
 'use strict';
 
-var assert = require("chai").assert;
+var actual;
+var expected;
 
 describe("Addition", function(){
 
     it("adds positive numbers", function(){
-        assert.equal(add(3,4), 7);
+        assertEqual(add(3,4), 7);
     });
 
     it("uses IEEE 754 floating point number", function(){
-        assert.equal(add(0.1,0.2), 0.30000000000000004);
+        assertEqual(add(0.1,0.2), 0.30000000000000004);
     });
 
+    function assertEqual(actual, expected){
+        if (actual !== expected) throw new Error("expected " + expected + ", but was " + actual);
+    }
 });
     function add(a, b) {
         return a + b;
